b2245a334a5654edadc4121a88d7d9a3e1919b49
Evan Boswell <evandboswell@gmail.com>
2019-09-23 09:54:00 -0400

    Temp Push

--- public/scripts.js
+++ public/scripts.js
@@ -39,21 +39,30 @@ function kidnap() {
         var u = a + b + c + d
         return u
     }
+    var James
     //This ajax code requests data from The Blue Alliance
     $.ajax({
-        url: url(), //This is the url we send to TBA which requests our data
+        url: url(),
         headers: {
             'X-TBA-Auth-Key': 'hhfIK2pSbl02wS0OQ2m593517wGnrSmjYdJ9C6jbMfKkvZtnoiB8qUWc0X0WwpC6' //This header contains Evan Boswell's Blue Alliance authentication key, this will need to be changed for years beyond 2019/2020
         },
         method: 'GET', //This defines the method we use to pull data from Blue Alliance, in this instance we are using GET
         dataType: 'json', //This defines what format the data that is pulled from Blue Alliance will be in, in this instance we are pulling Json files
-        success: function(data) { //this function logs our data in the console if it is successfully pulled
-            var James = (JSON.stringify(data));
-            return James;
-            
+        success: function (data) { //this function logs our data in the console if it is successfully pulled
+            James = (JSON.stringify(data));
+            //return James
+            console.log(James);
         }
     });
-    $(document).ajaxError(function() { //this function alerts an error if the pulling the data is unsuccessful
+
+    $(document).ajaxError(function () { //this function alerts an error if the pulling the data is unsuccessful
         alert("An error occurred!");
     });
+    function saveJSON(text, filename) {
+        var f = document.createElement('f');
+        f.setAttribute('href', 'data:text/plain;charset=utf-u,' + encodeURIComponent(text));
+        f.setAttribute('download', filename);
+        f.click();
+    }
+    saveJSON(JSON.stringify(data), 'pleasework.json');
 }
