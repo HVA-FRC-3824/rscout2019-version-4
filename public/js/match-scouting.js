a65a5d6f1ded4d98552a79cece5f2ef722baa51e
MaxHowell <maxhowell768@gmail.com>
2019-10-17 09:48:38 -0400

    started working on match scouting

---
+++ public/js/match-scouting.js
@@ -0,0 +1,7 @@
+var matchNumber;
+var teamNumber;
+function getMatchInfo() {
+    matchNumber = prompt("Enter Match number: ");
+    teamNumber = prompt("Team Number: ");
+    location.replace("./real-scouting.html");
+}
