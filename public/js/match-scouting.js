51866158ebe67d0b7496b792b4a17a10e76495f9
Evan Boswell <evandboswell@gmail.com>
2019-10-23 19:00:10 -0400

    Added back buttons and schedule page

--- public/js/match-scouting.js
+++ public/js/match-scouting.js
@@ -1,9 +1,14 @@
 var matchNumber;
 var teamNumber;
+
 function getMatchInfo() {
     matchNumber = prompt("Enter Match number: ");
     teamNumber = prompt("Team Number: ");
     location.replace("./real-scouting.html");
+}
+
+function goToSchedule() {
+    location.replace("./schedule.html");
 }
 
 function openPage(pageName, color) {
@@ -11,23 +16,25 @@ function openPage(pageName, color) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "";
+        tabcontent[i].style.display = "";
     }
-  
+
     // Remove the background color of all tablinks/buttons
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].style.backgroundColor = "purple";
+        tablinks[i].style.backgroundColor = "purple";
     }
-  
+
     // Show the specific tab content
     document.getElementById(pageName).style.display = "block";
-  
+
     // Add the specific color to the button used to open the tab content
     elmnt.style.backgroundColor = color;
 
     // Get the element with id="defaultOpen" and click on it
     document.getElementById("defaultOpen").click();
-  }
+}
 // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+document.getElementById("defaultOpen").click(); {
+
+}
