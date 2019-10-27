d79e327c3859abef8b586155afbd8bb03d2c161a
Evan Boswell <evandboswell@gmail.com>
2019-10-26 23:33:12 -0400

    Update analytics.js

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -1,51 +1,11 @@
 var James = null
-function kidnap() {
-    //These functions ask the user for information that will be used in The Blue Alliance url
-    function getCategory() {
-        var urlCategory = prompt("Enter Category: team, teams, or event");
-        //var urlCategory = document.getElementById("category").value;
-        if (urlCategory != "") { //This statement causes the function to return a null value if there is no input from the user
-            return "/" + urlCategory
-        } else {
-            return ""
-        };
-    };
-
-    function getID() {
-        var urlID = prompt("Enter team number or event id:");
-        //var urlID = document.getElementById("ID").value;
-        if (urlID != "") { //This statement causes the function to return a null value if there is no input from the user
-            return "/" + urlID
-        } else {
-            return ""
-        };
-    };
-
-    function getOutput() {
-        var urlOutput = prompt("Enter your output: awards, simple, teams");
-        //var urlOutput = document.getElementById("output").value;
-        if (urlOutput != "") { //This statement causes the function to return a null value if there is no input from the user
-            return "/" + urlOutput
-        } else {
-            return ""
-        };
-    };
-
+function kidnap(url) {
     //These variables store the data returned from the functions.
     var a = 'https://www.thebluealliance.com/api/v3'; //base TBA url
-    var b = getCategory();
-    var c = getID();
-    var d = getOutput();
-    //var e = '/awards'
-
-    //This function concatenates the variables with Blue Alliance's base url.
-    function url() {
-        var u = a + b + c + d
-        return u
-    };
+    var b = url;
     //This ajax code requests data from The Blue Alliance
     $.ajax({
-        url: url(), //This is the url we send to TBA which requests our data
+        url: a + b, //This is the url we send to TBA which requests our data
         headers: {
             'X-TBA-Auth-Key': 'hhfIK2pSbl02wS0OQ2m593517wGnrSmjYdJ9C6jbMfKkvZtnoiB8qUWc0X0WwpC6' //This header contains Evan Boswell's Blue Alliance authentication key, this will need to be changed for years beyond 2019/2020
         },
@@ -77,7 +37,7 @@ function tbaData() {
 };
 
 function makeSchedule() {
-    for (let i = 0; i < James.length; i++){
+    for (let i = 0; i < kidnap("/event/2019hop/matches"); i++){
         if (James.comp_level = "qm") {
             console.log(James[i]);
         };
