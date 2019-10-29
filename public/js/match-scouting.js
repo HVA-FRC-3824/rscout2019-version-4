06f241a36bec45eb3e507d15cd85fb1ddd331838
Evan Boswell <evandboswell@gmail.com>
2019-10-29 15:30:37 -0400

    Added correct numbers and match info to schedule

--- public/js/match-scouting.js
+++ public/js/match-scouting.js
@@ -1,21 +1,25 @@
-var matchNumber; teamNumber; data;
+var matchInfo; teamNumber; data; redAlliance; blueAlliance; 
 
+function createAlliance(i) {
+    redAlliance = James[i].alliances.blue.team_keys[0].slice(3) + " | " + James[i].alliances.blue.team_keys[1].slice(3) + " | " + James[i].alliances.blue.team_keys[2].slice(3);
+    blueAlliance = James[i].alliances.red.team_keys[0].slice(3) + " | " + James[i].alliances.red.team_keys[1].slice(3) + " | " + James[i].alliances.red.team_keys[2].slice(3);
+}3
 //Makes schedule
 function makeSchedule() {
-    kidnap("/event/2019hop/matches")
-    James.sort(sortById("match_number"))
-    //console.log(James);
-    var i;
-    for (i = 0; i < James.length; i++){
+    kidnap("/event/2019hop/matches");
+    James.sort(sortById("match_number"));
+    var i, k;
+    for (i = 0, k = 0; i < James.length; i++) {
         if (James[i].comp_level == "qm") {
-            matchNumber = ("Match " + i);
+            k++;
+            createAlliance(i);
+            matchInfo = ("Match " + k + ": " + redAlliance + " | vs | " + blueAlliance);
             btn = document.createElement("BUTTON");
-            btn.innerHTML = matchNumber;
+            btn.innerHTML = matchInfo;
             document.body.appendChild(btn);
         };
     };
 };
-
 
 /* ------------for real-scouting------------- */
 function openPage(pageName, color) {
