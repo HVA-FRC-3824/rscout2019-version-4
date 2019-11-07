cc33670783a85d1de7659c6c20bdd0c3ac89eaaf
Evan Boswell <evandboswell@gmail.com>
2019-11-07 09:53:08 -0500

    In progress update

--- public/js/match-scouting.js
+++ public/js/match-scouting.js
@@ -1,11 +1,9 @@
 var matchInfo;
-teamNumber;
-data;
-redAlliance;
-blueAlliance;
-k;
-btn;
-L;
+redAlliance = "";
+blueAlliance = "";
+k = 0;
+btn = "";
+mNumber = 0;
 matches = []; //* Initialize varibles
 
 function createAlliance(i) { //* This function creates each and concatenates each alliance number into a string
@@ -17,17 +15,28 @@ function makeSchedule() { //* Makes schedule
     kidnap("/event/2019hop/matches"); //* Runs kidnap with the specified url
     James.sort(sortById("match_number")); //* Sorts the output of the of kidnap by match number
     for (matchNumber = 0, k = 0; matchNumber < James.length; matchNumber++) { //* For loop for creating the schedule
-        if (James[matchNumber].comp_level == "qm") { //* If statement to exclude playoff matches from schedule
+        if (James[matchNumber].comp_level === "qm") { //* If statement to exclude playoff matches from schedule
             k++;
             createAlliance(matchNumber); //* Runs createAlliance to print match participants on the button
-            matchInfo = ("<a href=./real-scouting.html>Match " + k + ": " + redAlliance + " | vs | " + blueAlliance + "</a>"); //* Defines matchInfo as the text of a button
+            matchInfo = ("<button onclick = 'replacePage()'>Match " + k + ": " + redAlliance + " | vs | " + blueAlliance + "</button>"); //*Defines matchInfo as the text of a button
             btn = document.createElement("BUTTON"); //* creates a button
-            btn.innerHTML = matchInfo; //* Writes the matchInfo onto the button1
+            btn.id = k;
+            btn.innerHTML = matchInfo; //* Writes the matchInfo onto the button
             document.body.appendChild(btn);
-            
         };
     };
 };
+
+function mNumber() {
+    mNumber = btn.getElementById("BUTTON");
+    console.log(mNumber);
+}
+
+function replacePage() {
+    location.replace("./real-scouting.html");
+    mNumber();
+    //mNumber = btn.getElementById(id);
+}
 
 /* ------------for real-scouting------------- */
 function openPage(pageName, color) {
@@ -49,9 +58,9 @@ function openPage(pageName, color) {
 
     //* Add the specific color to the button used to open the tab content
     elmnt.style.backgroundColor = color;
-
 };
 //* Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+//document.getElementById("defaultOpen").click();
+document.querySelector('.content .value').innerHTML = mNumber;
 
 /* ------------for real-scouting------------- */
