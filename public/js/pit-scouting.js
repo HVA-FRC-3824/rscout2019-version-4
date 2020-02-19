9e9821becd7ecc6eba7252848833e55dd3e3c923
Grantarg <trainfantn@live.com>
2020-02-19 11:20:28 -0500

    pit scouting and stuff

--- public/js/pit-scouting.js
+++ public/js/pit-scouting.js
@@ -19,3 +19,11 @@ function addPitToFirebase(data) {
         "DriveTrain": data.driveTrain,
     });
 }
+
+function isNumberKey(evt) {
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+
+    return true;
+}
