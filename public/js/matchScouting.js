bb20399795da2d5d69a488d40eb0d056991071ac
Weston <weston.agreda@gmail.com>
2021-01-12 20:36:56 -0500

    Trying to make the schedule work
    we will get there eventually

--- public/js/matchScouting.js
+++ public/js/matchScouting.js
@@ -1,13 +1,70 @@
-//TODO reference data.js 
+let James = {};
 
-//TODO Pre match
+function loadSchedule() {
+    James = JSON.parse(localStorage.getItem("blueAllianceData"));
+    James.sort(sortById("match_number")); //* Sorts the output of the of kidnap by match number
+    for (matchNumber = 1; matchNumber <= James.length; matchNumber++) { //* For loop for creating the schedule
+        createAlliance(matchNumber); //* Runs createAlliance to print match participants on the button
+        matchInfo = ("<button onclick =  'startMatchScouting(" + matchNumber + "," + JSON.stringify(James[matchNumber - 1].alliances) + ")'> Match " + matchNumber + ": <p style='color:red'>" + redAlliance + "</p> | vs | <p style='color:blue'>" + blueAlliance + "</p></button>"); //*Defines matchInfo as the text of a button
+        btn = document.createElement("BUTTON"); //* creates a button
+        btn.innerHTML = matchInfo; //* Writes the matchInfo onto the button
+        document.body.appendChild(btn);
+    };
+}
 
-//TODO Auto
+function kidnap(newUrl) {
+    //These variables store the data returned from the functions.
+    var baseUrl = 'https://www.thebluealliance.com/api/v3'; //base TBA url
+    //This ajax code requests data from The Blue Alliance
+    $.ajax({
+        url: baseUrl + newUrl, //This is the url we send to TBA which requests our data
+        headers: {
+            'X-TBA-Auth-Key': 'hhfIK2pSbl02wS0OQ2m593517wGnrSmjYdJ9C6jbMfKkvZtnoiB8qUWc0X0WwpC6' //This header contains Evan Boswell's Blue Alliance authentication key, this will need to be changed for years beyond 2019/2020
+        },
+        method: 'GET', //This defines the method we use to pull data from Blue Alliance, in this instance we are using GET
+        dataType: 'json', //This defines what format the data that is pulled from Blue Alliance will be in, in this instance we are pulling Json files
+        success: function(data) { //this function logs our data in the console if it is successfully pulled
+            James = data;
+            console.log(James);
+            return James;
 
-//TODO Teleop
+        },
+    });
+    $(document).ajaxError(function() { //this function alerts an error if the pulling the data is unsuccessful
+        alert("An error occurred!");
+    });
 
-//TODO Endgame
+    console.log(James);
+};
 
-//TODO Post match info
+var sortById = function(prop) {
+    return function(x, y) {
+        return ((x[prop] === y[prop]) ? 0 : ((x[prop] > y[prop]) ? 1 : -1));
+    };
+};
 
-//TODO push match data to firebase
+
+function makeSchedule() { //* Makes schedule
+
+    //* Checks localStorage for the last match scouted 
+    previousMatch = localStorage.getItem("previousMatch");
+    if (previousMatch == null) {
+        previousMatch = 1;
+    }
+    console.log(previousMatch);
+    //* 
+
+    kidnap("/event/2020scmb/matches"); //* Runs kidnap with the specified url
+    James.sort(sortById("match_number")); //* Sorts the output of the of kidnap by match number
+    filteredJames = James.filter(filterSchedule);
+    var i = filteredJames.length;
+    document.body.innerHTML = "<button onclick=makeSchedule() class='button1'> Populate Matches </button> <br>        <form action='./index.html'>    <button type='submit' class='buttonBack'>Back</button></form>";
+    for (matchNumber = previousMatch; matchNumber <= i; matchNumber++) { //* For loop for creating the schedule
+        createAlliance(matchNumber); //* Runs createAlliance to print match participants on the button
+        matchInfo = ("<button onclick =  'startMatchScouting(" + matchNumber + "," + JSON.stringify(filteredJames[matchNumber - 1].alliances) + ")'> Match " + matchNumber + ": <p style='color:#C1666B'>" + redAlliance + "</p> vs <p style='color:#4357AD'>" + blueAlliance + "</p></button>"); //*Defines matchInfo as the text of a button
+        btn = document.createElement("BUTTON"); //* creates a button
+        btn.innerHTML = matchInfo; //* Writes the matchInfo onto the button
+        document.body.appendChild(btn);
+    };
+    localStorage.setItem("blueAllianceData", JSON.stringify(filteredJames));
+};
