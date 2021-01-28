2347cd9e6c013991c6f056c424da3225932edc07
Weston <weston.agreda@gmail.com>
2021-01-28 04:36:54 -0500

    match preview works fr this time

--- public/js/preview.js
+++ public/js/preview.js
@@ -4,30 +4,34 @@ var teamSide = "red";
 var robotNumber = 9999;
 
 function pullMatchData() {
-    matchNumber = document.getElementById('matchNumPreview').value;
-    console.log(matchNumber)
     kidnap("/event/2020scmb/matches");
     James.sort(sortById("match_number"));
-    var filteredJames = James.filter(filterSchedule);
+    globalThis.filteredJames = James.filter(filterSchedule);
+    console.log(filteredJames);
 }
 
 function pullMatchInput() {
+    matchNumber = document.getElementById('matchNumPreview').value;
+    dStation = 0;
+    teamSide = "red";
+    pullMatch(matchNumber);
+}
+
+function pullMatch(matchNumber) {
     if (teamSide == "red") {
         console.log("this is red");
+        //console.log(filteredJames);
         robotNumber = filteredJames[matchNumber - 1].alliances.red.team_keys[dStation].slice(3);
     } else {
         console.log("this is blue");
         robotNumber = filteredJames[matchNumber - 1].alliances.blue.team_keys[dStation].slice(3);
     }
-    pullMatch(robotNumber);
-}
-
-function pullMatch(robotNumber) {
     pullPreviewData(robotNumber);
 }
 
 //gets the json from firebase of a certain robot
 function pullPreviewData(robotNumber) {
+    console.log(robotNumber);
     firebase.database().ref('/matchScouting/' + robotNumber).once("value", gotMatchData);
 }
 
@@ -39,26 +43,20 @@ function gotMatchData(data) { //makes the data readable
     //gets us some data from firebase
     var matchParsed = JSON.parse(JSON.stringify(data.val()));
     var matchNums = Object.keys(matchParsed);
-    for (let i = 0; i < matchNums.length; i++) { //couple for loops to grab alllll the data for that robot
+    for (i = 0; i < matchNums.length; i++) { //couple for loops to grab alllll the data for that robot
         //console.log(matchNums);
         var currMatch = matchNums[i];
         var matchNames = Object.keys(matchParsed[currMatch]);
         //console.log(matchNames);
-        for (let i = 0; i < matchNames.length; i++) {
-            currName = matchNames[i];
+        for (j = 0; j < matchNames.length; j++) {
+            currName = matchNames[j];
             //console.log("MatchData: " + currMatch + " " + currentName);
             //ALL DATA WE WANT GOES HERE
             teleAccuracyMaster.push(matchParsed[currMatch][currName]["teleAccuracy"]);
             climbTypeMaster.push(matchParsed[currMatch][currName]["climbType"]);
-            /*
-            //A:: DATA WE WANT GOES HERE
-            for (l = 0; l < 500; l++) {
-                console.log("Loading...");
-            } //might not need this but its here because im scared it will break things again
-            */
         }
     }
-    for (t = 0; t < (teleAccuracyMaster.length); t++) { //to calculate averages or compline data, could be used for other things that just tele accuracy master
+    for (t = 0; t < (teleAccuracyMaster.length); t++) { //to calculate averages or combine data, could be used for other things that just tele accuracy master
         teleAccuracyTotal += teleAccuracyMaster[t];
     }
 
@@ -68,35 +66,40 @@ function gotMatchData(data) { //makes the data readable
     switch (teamStation) {
         case "0r":
             document.getElementById("r1").innerHTML = robotNumber;
-            //document.getElementById("red1Data").innerHTML = "Tele Accuracy: " + (Math.round(((teleAccuracyTotal / teleAccuracyMaster.length) + Number.EPSILON) * 100) / 100) + climbTypeMaster;
+            document.getElementById("red1Data").innerHTML = "Tele Accuracy: " + (Math.round(((teleAccuracyTotal / teleAccuracyMaster.length) + Number.EPSILON) * 100) / 100);
             dStation = 1;
             pullMatch(document.getElementById("matchNumPreview").value);
+            break;
         case "1r":
             document.getElementById("r2").innerHTML = robotNumber;
-            //document.getElementById("red2Data").innerHTML = "Tele Accuracy: " + (Math.round(((teleAccuracyTotal / teleAccuracyMaster.length) + Number.EPSILON) * 100) / 100) + climbTypeMaster;
+            document.getElementById("red2Data").innerHTML = "Tele Accuracy: " + (Math.round(((teleAccuracyTotal / teleAccuracyMaster.length) + Number.EPSILON) * 100) / 100);
             dStation = 2;
             pullMatch(document.getElementById("matchNumPreview").value);
+            break;
         case "2r":
             document.getElementById("r3").innerHTML = robotNumber;
-            //document.getElementById("red3Data").innerHTML = "Tele Accuracy: " + (Math.round(((teleAccuracyTotal / teleAccuracyMaster.length) + Number.EPSILON) * 100) / 100) + climbTypeMaster;
+            document.getElementById("red3Data").innerHTML = "Tele Accuracy: " + (Math.round(((teleAccuracyTotal / teleAccuracyMaster.length) + Number.EPSILON) * 100) / 100);
             dStation = 0;
             switchColor = (filteredJames[matchNumber - 1].alliances.blue.team_keys[0].slice(3))
-            teamSide = blue;
+            teamSide = "blue";
             pullMatch(document.getElementById("matchNumPreview").value);
+            break;
         case "0b":
             document.getElementById("b1").innerHTML = robotNumber;
-            //document.getElementById("blue1Data").innerHTML = "Tele Accuracy: " + (Math.round(((teleAccuracyTotal / teleAccuracyMaster.length) + Number.EPSILON) * 100) / 100) + climbTypeMaster;
+            document.getElementById("blue1Data").innerHTML = "Tele Accuracy: " + (Math.round(((teleAccuracyTotal / teleAccuracyMaster.length) + Number.EPSILON) * 100) / 100);
             dStation = 1;
             pullMatch(document.getElementById("matchNumPreview").value);
+            break;
         case "1b":
             document.getElementById("b2").innerHTML = robotNumber;
-            //document.getElementById("blue2Data").innerHTML = "Tele Accuracy: " + (Math.round(((teleAccuracyTotal / teleAccuracyMaster.length) + Number.EPSILON) * 100) / 100) + climbTypeMaster;
+            document.getElementById("blue2Data").innerHTML = "Tele Accuracy: " + (Math.round(((teleAccuracyTotal / teleAccuracyMaster.length) + Number.EPSILON) * 100) / 100);
             dStation = 2;
             pullMatch(document.getElementById("matchNumPreview").value);
+            break;
         case "2b":
             document.getElementById("b3").innerHTML = robotNumber;
-            //document.getElementById("blue3data").innerHTML = "Tele Accuracy: " + (Math.round(((teleAccuracyTotal / teleAccuracyMaster.length) + Number.EPSILON) * 100) / 100) + climbTypeMaster;
-            pullMatch(document.getElementById("matchNumPreview").value);
+            document.getElementById("blue3Data").innerHTML = "Tele Accuracy: " + (Math.round(((teleAccuracyTotal / teleAccuracyMaster.length) + Number.EPSILON) * 100) / 100);
+            break;
     }
     //
     //THIS WILL ACTUALLY PUT THE DATA ON SCREEN, PUT ALL THE DATA ON THERE AT ONCE BY PUTTING IT 
--- public/js/schedule.js
+++ public/js/schedule.js
@@ -1,4 +1,4 @@
-var filteredJames = [];
+//var filteredJames = [];
 
 function startMatchScouting(mNumber, alliances) {
     localStorage.setItem("num", mNumber);
@@ -6,21 +6,6 @@ function startMatchScouting(mNumber, alliances) {
     location.replace("./matchScouting.html");
 };
 
-function loadSchedule() {
-    James = JSON.parse(localStorage.getItem("blueAllianceData"));
-    James.sort(sortById("match_number")); //* Sorts the output of the of kidnap by match number
-    for (matchNumber = 1; matchNumber <= James.length; matchNumber++) { //* For loop for creating the schedule
-        createAlliance(matchNumber); //* Runs createAlliance to print match participants on the button
-        matchInfo = ("<button onclick =  'startMatchScouting(" + matchNumber + "," +
-            JSON.stringify(James[matchNumber - 1].alliances) + ")'> Match " + matchNumber +
-            ": <p style='color:red'>" + redAlliance + "</p> | vs | <p style='color:blue'>" + blueAlliance +
-            "</p></button>"); //*Defines matchInfo as the text of a button
-        btn = document.createElement("BUTTON"); //* creates a button
-        btn.innerHTML = matchInfo; //* Writes the matchInfo onto the button
-        document.body.appendChild(btn);
-    };
-}
-
 function makeSchedule() { //* Makes schedule
     //* Checks localStorage for the last match scouted 
     previousMatch = localStorage.getItem("previousMatch");
@@ -33,7 +18,7 @@ function makeSchedule() { //* Makes schedule
     James.sort(sortById("match_number")); //* Sorts the output of the of kidnap by match number
     filteredJames = James.filter(filterSchedule);
     var i = filteredJames.length;
-    document.body.innerHTML = "<button onclick=makeSchedule() class='button1'> Populate Matches </button>      <form action='./index.html'>    <button type='submit' class='backBtn'>Back</button></form><br>";
+    document.body.innerHTML = "<form action='./index.html'>    <button type='submit' class='backBtn'>Back</button></form><br>";
     for (matchNumber = previousMatch; matchNumber <= i; matchNumber++) { //* For loop for creating the schedule
         createAlliance(matchNumber); //* Runs createAlliance to print match participants on the button
         matchInfo = ("<button class='scheduleButton'  onclick =  'startMatchScouting(" + matchNumber + "," + JSON.stringify(filteredJames[matchNumber - 1].alliances) + ")'> Match " + matchNumber + " <br>________________ <p style='color:#C80815'>" + redAlliance + "</p> vs <p style='color:#0075b3'>" + blueAlliance + "</p></button>"); //*Defines matchInfo as the text of a button
@@ -50,68 +35,6 @@ function createAlliance(matchNumber) { //* This function creates each and concat
     redAlliance = filteredJames[i].alliances.red.team_keys[0].slice(3) + " | " + filteredJames[i].alliances.red.team_keys[1].slice(3) + " | " + filteredJames[i].alliances.red.team_keys[2].slice(3);
 }
 
-
-function makeSchedule() { //* Makes schedule
-    //* Checks localStorage for the last match scouted 
-    previousMatch = localStorage.getItem("previousMatch");
-    if (previousMatch == null) {
-        previousMatch = 1;
-    }
-    console.log(previousMatch);
-    //* 
-
-    kidnap("/event/2020scmb/matches"); //* Runs kidnap with the specified url
-    James.sort(sortById("match_number")); //* Sorts the output of the of kidnap by match number
-    filteredJames = James.filter(filterSchedule);
-    var i = filteredJames.length;
-    document.body.innerHTML = "<button onclick=makeSchedule() class='button1'> Populate Matches </button>      <form action='./index.html'>    <button type='submit' class='backBtn'>Back</button></form><br>";
-    for (matchNumber = previousMatch; matchNumber <= i; matchNumber++) { //* For loop for creating the schedule
-        createAlliance(matchNumber); //* Runs createAlliance to print match participants on the button
-        matchInfo = ("<button class='scheduleButton'  onclick =  'startMatchScouting(" + matchNumber + "," + JSON.stringify(filteredJames[matchNumber - 1].alliances) + ")'> Match " + matchNumber + " <br>________________ <p style='color:#C80815'>" + redAlliance + "</p> vs <p style='color:#0075b3'>" + blueAlliance + "</p></button>"); //*Defines matchInfo as the text of a button
-        btn = document.createElement("DIV"); //* creates a button
-        btn.innerHTML = matchInfo; //* Writes the matchInfo onto the button
-        document.body.appendChild(btn);
-    };
-    localStorage.setItem("blueAllianceData", JSON.stringify(filteredJames));
-};
-
-function createAlliance(matchNumber) { //* This function creates each and concatenates each alliance number into a string
-    var i = matchNumber - 1
-    blueAlliance = filteredJames[i].alliances.blue.team_keys[0].slice(3) + " | " + filteredJames[i].alliances.blue.team_keys[1].slice(3) + " | " + filteredJames[i].alliances.blue.team_keys[2].slice(3);
-    redAlliance = filteredJames[i].alliances.red.team_keys[0].slice(3) + " | " + filteredJames[i].alliances.red.team_keys[1].slice(3) + " | " + filteredJames[i].alliances.red.team_keys[2].slice(3);
-}
-
-
-function makeSchedule() { //* Makes schedule
-
-    //* Checks localStorage for the last match scouted 
-    previousMatch = localStorage.getItem("previousMatch");
-    if (previousMatch == null) {
-        previousMatch = 1;
-    }
-    console.log(previousMatch);
-    //* 
-
-    kidnap("/event/2020scmb/matches"); //* Runs kidnap with the specified url
-    James.sort(sortById("match_number")); //* Sorts the output of the of kidnap by match number
-    filteredJames = James.filter(filterSchedule);
-    var i = filteredJames.length;
-    document.body.innerHTML = "<button onclick=makeSchedule() class='button1'> Populate Matches </button>      <form action='./index.html'>    <button type='submit' class='backBtn'>Back</button></form><br>";
-    for (matchNumber = previousMatch; matchNumber <= i; matchNumber++) { //* For loop for creating the schedule
-        createAlliance(matchNumber); //* Runs createAlliance to print match participants on the button
-        matchInfo = ("<button class='scheduleButton'  onclick =  'startMatchScouting(" + matchNumber + "," + JSON.stringify(filteredJames[matchNumber - 1].alliances) + ")'> Match " + matchNumber + " <br>________________ <p style='color:#C80815'>" + redAlliance + "</p> vs <p style='color:#0075b3'>" + blueAlliance + "</p></button>"); //*Defines matchInfo as the text of a button
-        btn = document.createElement("DIV"); //* creates a button
-        btn.innerHTML = matchInfo; //* Writes the matchInfo onto the button
-        document.body.appendChild(btn);
-    };
-    localStorage.setItem("blueAllianceData", JSON.stringify(filteredJames));
-};
-
-function createAlliance(matchNumber) { //* This function creates each and concatenates each alliance number into a string
-    var i = matchNumber - 1
-    blueAlliance = filteredJames[i].alliances.blue.team_keys[0].slice(3) + " | " + filteredJames[i].alliances.blue.team_keys[1].slice(3) + " | " + filteredJames[i].alliances.blue.team_keys[2].slice(3);
-    redAlliance = filteredJames[i].alliances.red.team_keys[0].slice(3) + " | " + filteredJames[i].alliances.red.team_keys[1].slice(3) + " | " + filteredJames[i].alliances.red.team_keys[2].slice(3);
-}
 
 function filterSchedule(qual) {
     return qual.comp_level == "qm";
