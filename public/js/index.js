9783c8481c62e09904a2b650ede015f371548406
grant <trainfantn@live.com>
2021-01-28 23:33:25 -0500

    init to new auth branch

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,30 @@
 const auth = firebase.auth();
 const auth = firebase.firestore();
+
+// ====================== for the menu onclick buttons ============================= //
+
+var analyticsBttn = document.getElementById('analyticsBttn');
+matchBttn = document.getElementById('matchBttn');
+pitBttn = document.getElementById('pitBttn');
+superBttn = document.getElementById('superBttn');
+previewBttn = document.getElementById('previewBttn');
+
+analyticsBttn.onclick = function() {
+
+}
+
+matchBttn.onclick = function() {
+
+}
+
+pitBttn.onclick = function() {
+
+}
+
+superBttn.onclick = function() {
+
+}
+
+previewBttn.onclick = function() {
+    
+}
